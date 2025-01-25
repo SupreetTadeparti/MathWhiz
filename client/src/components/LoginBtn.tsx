9e784428d32872fd9cacd07725428d4bce04a60e
Rishabh Fuke <rishabhfuke@Rishabhs-MacBook-Air.local>
2025-01-25 15:22:27 -0500

    Configured the login button to work with Auth0

--- client/src/components/LoginBtn.tsx
+++ client/src/components/LoginBtn.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginBtn: React.FC = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
-    <div className="text-white text-lg border-white rounded-sm border-2 px-6 py-1 cursor-pointer bg-transparent hover:bg-white hover:text-gray-800 transition-colors duration-300">
+    <div 
+    onClick={() => loginWithRedirect()}
+    className="text-white text-lg border-white rounded-sm border-2 px-6 py-1 cursor-pointer bg-transparent hover:bg-white hover:text-gray-800 transition-colors duration-300">
       Login
     </div>
   );
