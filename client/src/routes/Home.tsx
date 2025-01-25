5237d56121b4443eb903b34d3100703a9289a29c
d4julian <julianubico@gmail.com>
2025-01-25 17:38:14 -0500

    Update Home.tsx

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,24 +1,29 @@
-import React from "react";
+import { React, useState } from "react";
 import Navbar from "../components/Navbar";
 import Background from "../components/Background";
 import CoverButton from "../components/CoverButton";
 
 const Home: React.FC = () => {
+  const [videoPath, setVideoPath] = useState(null);
+
   return (
     <>
       <Navbar />
       <Background />
-      <div className="container flex flex-col justify-center items-center gap-10 w-full h-full ">
-        <video width="640" height="320" controls autoPlay>
+      <div className="flex flex-col justify-center items-center gap-20 w-full h-full ">
+        <div className="flex flex-col gap-10 p-20 bg-black/40 rounded-md border border-gray-100">
+          <input
+            className="w-80 text-white text-center text-lg py-1 px-2 border-b-2 border-white focus:outline-none"
+            type="text"
+            placeholder="What would you like to understand?"
+          />
+          <CoverButton content="Generate Visual Explanation" />
+        </div>
+
+        {videoPath && <video width="640" height="320" controls autoPlay>
           <source src="animation.mp4" type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
-        <input
-          className="w-80  text-white text-center text-lg py-1 px-2 border-b-2 border-white focus:outline-none"
-          type="text"
-          placeholder="What would you like to understand?"
-        />
-        <CoverButton content="Generate Visual Explanation" />
+        </video>}
       </div>
     </>
   );
