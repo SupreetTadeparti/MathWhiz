40b638c8feb8f97a25f6a4c8056ad673b110f219
Supreet T <supsrasri@gmail.com>
2025-01-25 14:30:16 -0500

    navbar styles

--- client/src/components/LoginBtn.tsx
+++ client/src/components/LoginBtn.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const LoginBtn: React.FC = () => {
   return (
-    <div className="login-btn">Login</div>
+    <div className="text-white text-lg border-white rounded-sm border-2 px-6 py-1 cursor-pointer bg-transparent hover:bg-white hover:text-gray-800 transition-colors duration-300">
+      Login
+    </div>
   );
-}
+};
 
 export default LoginBtn;
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import LoginBtn from "./LoginBtn";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="fixed w-full h-20 bg-red-400 flex justify-end">
-      <div className="logo">Omegagen</div>
+    <div className="fixed w-full h-20 flex justify-end items-center gap-10 px-10">
+      <div className="mr-auto text-3xl text-cyan-200 font-bold">Animodulus</div>
       <LoginBtn />
       <div className="h-8 flex flex-col justify-between cursor-pointer">
         <div className="menu-line"></div>
