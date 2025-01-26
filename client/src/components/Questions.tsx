a20cc0d873793e6073b9850879b9437f89f1d8cd
d4julian <julianubico@gmail.com>
2025-01-26 08:40:43 -0500

    merge

--- client/src/components/Questions.tsx
+++ client/src/components/Questions.tsx
@@ -4,7 +4,6 @@ interface QuestionsProps {
 
 const Questions: React.FC<QuestionsProps> = ({ questionData }) => {
   return (
-<<<<<<< HEAD
     <div className="question-container px-25 py-25 flex flex-col gap-15">
       <div className="question">{questionData[currQuestion].question}</div>
       <div className="options grid grid-cols-2 place-items-center gap-y-15">
@@ -20,15 +19,6 @@ const Questions: React.FC<QuestionsProps> = ({ questionData }) => {
         <div onClick={() => selectOption("d")} className={getOptionClass("d")}>
           {questionData[currQuestion].options.d}
         </div>
-=======
-    <div>
-      <div className="question">questionData</div>
-      <div className="options">
-        <div className="option"></div>
-        <div className="option"></div>
-        <div className="option"></div>
-        <div className="option"></div>
->>>>>>> 0c3fa9e06bde70f1d9621cc4717ad151d5033f68
       </div>
     </div>
   );
