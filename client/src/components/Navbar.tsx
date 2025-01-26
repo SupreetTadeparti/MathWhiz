990b67d156f69517d3a841460cb7a2c294aae545
L <lcsreilly@gmail.com>
2025-01-25 22:38:17 -0500

    menu2!

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,36 +2,52 @@ import React, { useState } from "react";
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar: React.FC = () => {
+  const { logout } = useAuth0();
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const handleMouseEnter = () => {
+    setMenuOpen(true);
   };
 
-  const { logout } = useAuth0();
+  const handleMouseLeave = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <div className="fixed w-full h-20 flex justify-center items-center gap-10 px-10">
       <img className="mr-auto h-8" src="/img/logo.svg" alt="Mathwhiz.biz" />
 
-      <div className="h-8 flex flex-col justify-between cursor-pointer" onClick={toggleMenu}>
+      <div
+        className="h-8 flex flex-col justify-between cursor-pointer"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <div className="menu-line"></div>
         <div className="menu-line"></div>
         <div className="menu-line"></div>
       </div>
-      {menuOpen && (
-        <div className="absolute top-20 right-10 bg-[var(--secondary)] shadow-lg rounded-lg border-2 border-yellow-500">
-          <ul className="flex flex-col p-2">
-            <li className="py-4 px-6 text-white text-xl text-center rounded-sm cursor-pointer bg-transparent hover:bg-[var(--primary)] transition-colors duration-300">Option 1</li>
-            <li className="py-4 px-6 text-white text-xl text-center rounded-sm cursor-pointer bg-transparent hover:bg-[var(--primary)] transition-colors duration-300">Option 2</li>
-            <li
-              onClick={() => logout()}
-              className="py-4 px-6 text-white text-xl rounded-sm cursor-pointer bg-transparent hover:bg-[var(--primary)] transition-colors duration-300"
-            >Log out
-            </li>
-          </ul>
-        </div>
-      )}
+      <div
+        className={`absolute top-5 right-5 bg-[var(--secondary)] shadow-lg rounded-lg border-2 border-yellow-500 transition-opacity duration-50 ${
+          menuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <ul className="flex flex-col p-2">
+          <li className="py-4 px-6 text-white text-xl text-center rounded-sm cursor-pointer bg-transparent hover:bg-[var(--primary)] hover:text-[var(--secondary)] transition-colors duration-300">
+            Option 1
+          </li>
+          <li className="py-4 px-6 text-white text-xl text-center rounded-sm cursor-pointer bg-transparent hover:bg-[var(--primary)] hover:text-[var(--secondary)] transition-colors duration-300">
+            Option 2
+          </li>
+          <li
+            onClick={() => logout()}
+            className="py-4 px-6 text-white text-xl rounded-sm cursor-pointer bg-transparent hover:bg-[var(--primary)] hover:text-[var(--secondary)] transition-colors duration-300"
+          >
+            Log out
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
