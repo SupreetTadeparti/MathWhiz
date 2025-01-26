64554bcfdad050f820f7a63d63b03cfa67a515c7
Supreet T <supsrasri@gmail.com>
2025-01-26 04:16:42 -0500

    modified env

--- client/supabase.ts
+++ client/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zuyhxxleysvexxokhteo.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1eWh4eGxleXN2ZXh4b2todGVvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNzg3ODU2OSwiZXhwIjoyMDUzNDU0NTY5fQ.dpgskPGNz06pr9QCMZ9Uk8iul7tvijfIYeiTBkkmZAM";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
